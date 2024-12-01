4284bcb3cedb94cccdf8a9b526fede3536589979
fernandosuertee <fernandosuertemiranda@gmail.com>
2024-12-01 00:03:02 -0300

    spring security11

--- src/main/java/com/uniamerica/pie/hotel/models/Usuario.java
+++ src/main/java/com/uniamerica/pie/hotel/models/Usuario.java
@@ -46,25 +46,21 @@ public class Usuario implements UserDetails{
 
 	@Override
 	public boolean isAccountNonExpired() {
-		// TODO Auto-generated method stub
 		return true;
 	}
 
 	@Override
 	public boolean isAccountNonLocked() {
-		// TODO Auto-generated method stub
 		return true;
 	}
 
 	@Override
 	public boolean isCredentialsNonExpired() {
-		// TODO Auto-generated method stub
 		return true;
 	}
 
 	@Override
 	public boolean isEnabled() {
-		// TODO Auto-generated method stub
 		return true;
 	}
 
